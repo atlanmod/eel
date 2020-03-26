c10167f797d9b4fd553aafccf6f5e4933a4cd03b
Thibault Béziers la Fosse <thiblf@gmail.com>
2020-03-26 14:25:17 +0100

    Updated the code generator, and added a more complex example using light sensors

---
+++ Language_Workbench/fr.obeo.dsl.arduino.gen/models/photoResitorTriggersLedWhenLow.ino
@@ -0,0 +1,27 @@
+#include<avr/sleep.h>
+int blueLed = 9;
+int sensor = 0;
+
+void setup() {
+	pinMode(blueLed, OUTPUT);
+}
+		
+// the loop routine runs over and over again forever:
+void loop() {
+	int lightCal = 0; 
+	lightCal = digitalRead(sensor); 
+	while (true) {
+		int lightVal = 0; 
+		lightVal = digitalRead(sensor); 
+		int minLightChange = 0; 
+		minLightChange = lightCal  - 50; 
+		if (lightVal  < minLightChange) {
+			digitalWrite(blueLed, 1);
+		} else {
+			digitalWrite(blueLed, 0);
+		} 
+		delay(2000);
+	}
+}
+
+
